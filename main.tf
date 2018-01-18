852a25e92b8914ed09c9c1614635bbd343273b2d
NKuzmin <nkuzmin@at-consulting.ru>
2018-01-18 06:00:13 +0300

    initial commit

---
+++ main.tf
@@ -0,0 +1,24 @@
+provider "google" {
+  project = "infra-192502"
+
+  region = "europe-west1"
+}
+
+resource "google_compute_instance" "app" {
+  name = "reddit-app"
+  machine_type = "g1-small"
+  zone = "europe-west1-b"
+  # определение загрузочного диска
+  boot_disk {
+    initialize_params {
+      image = "reddit-base-1504639663"
+    }
+  }
+  # определение сетевого интерфейса
+  network_interface {
+    # сеть, к которой присоединить данный интерфейс
+    network = "default"
+    # использовать ephemeral IP для доступа из Интернет
+    access_config {}
+  }
+}
