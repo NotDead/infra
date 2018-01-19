81adee3f3697d54ba92a7f8d5433f22048e93658
NKuzmin <nkuzmin@at-consulting.ru>
2018-01-19 03:47:44 +0300

    added more tf files

--- main.tf
+++ main.tf
@@ -7,18 +7,49 @@ provider "google" {
 resource "google_compute_instance" "app" {
   name = "reddit-app"
   machine_type = "g1-small"
-  zone = "europe-west1-b"
+  zone = "europe-west3-c"
+  tags = ["reddit-app"]
   # определение загрузочного диска
   boot_disk {
     initialize_params {
-      image = "reddit-base-1504639663"
+      image = "reddit-base-1516246236"
     }
   }
   # определение сетевого интерфейса
+  metadata {
+    sshKeys = "appuser:${file("C:/Users/NKuzmin/.ssh/appuser.pub")}"
+  }
   network_interface {
     # сеть, к которой присоединить данный интерфейс
     network = "default"
     # использовать ephemeral IP для доступа из Интернет
     access_config {}
   }
+  connection {
+    type = "ssh"
+    user = "appuser"
+    agent = false
+    private_key = "${file("C:/Users/NKuzmin/.ssh/appuser")}"
+  }
+  provisioner "file" {
+    source = "files/puma.service"
+    destination = "/tmp/puma.service"
+  }
+  provisioner "remote-exec" {
+    script = "files/deploy.sh"
+  }
+}
+resource "google_compute_firewall" "firewall_puma" {
+  name = "allow-puma-default"
+  # Название сети, в которой действует правило
+  network = "default"
+  # Какой доступ разрешить
+  allow {
+    protocol = "tcp"
+    ports = ["9292"]
+  }
+  # Каким адресам разрешаем доступ
+  source_ranges = ["0.0.0.0/0"]
+  # Правило применимо для инстансов с тегом ...
+  target_tags = ["reddit-app"]
 }
